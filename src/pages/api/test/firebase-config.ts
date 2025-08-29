d1c9feb8eea4904b138bb6a9fd637f06459c725e
Ville Takanen <vitkukissa@gmail.com>
2025-08-29 10:06:08 +0300

    fix: thread creation in serverside

---
+++ src/pages/api/test/firebase-config.ts
@@ -0,0 +1,10 @@
+export async function GET() {
+  return new Response(JSON.stringify({
+    apiKey: import.meta.env.PUBLIC_apiKey,
+    authDomain: import.meta.env.PUBLIC_authDomain,
+    projectId: import.meta.env.PUBLIC_projectId,
+    storageBucket: import.meta.env.PUBLIC_storageBucket,
+  }), {
+    headers: { 'Content-Type': 'application/json' }
+  });
+}
