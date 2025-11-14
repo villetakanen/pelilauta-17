9c6e4e4e7e50885c7e3170af87d74368ef41e3f6
Ville Takanen <vitkukissa@gmail.com>
2025-11-14 14:54:49 +0200

    refactor(page): use SiteSchema.parse() in addPageRef

--- src/firebase/client/page/addPageRef.ts
+++ src/firebase/client/page/addPageRef.ts
@@ -1,18 +1,21 @@
 import {
   type PageRef,
-  parseSite,
+  SiteSchema,
   SITES_COLLECTION_NAME,
-} from 'src/schemas/SiteSchema';
-import { toClientEntry } from 'src/utils/client/entryUtils';
-import { db } from '..';
-import { updateSiteApi } from '../site/updateSiteApi';
+} from "src/schemas/SiteSchema";
+import { toClientEntry } from "src/utils/client/entryUtils";
+import { db } from "..";
+import { updateSiteApi } from "../site/updateSiteApi";
 
 export async function addPageRef(pageRef: PageRef, siteKey: string) {
   // Get the siteDoc and Site from the firestore
-  const { getDoc, doc } = await import('firebase/firestore');
+  const { getDoc, doc } = await import("firebase/firestore");
   const siteDoc = await getDoc(doc(db, SITES_COLLECTION_NAME, siteKey));
-  if (!siteDoc.exists()) throw new Error('addPageRef: Site not found');
-  const site = parseSite(toClientEntry(siteDoc.data()), siteKey);
+  if (!siteDoc.exists()) throw new Error("addPageRef: Site not found");
+  const site = SiteSchema.parse({
+    ...toClientEntry(siteDoc.data()),
+    key: siteKey,
+  });
 
   // Clone the pageRefs array and add the new pageRef
   const refs: PageRef[] = site.pageRefs ? [...site.pageRefs] : [];
